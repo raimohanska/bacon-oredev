cc081d349e0a44c5514f04b31f3408ff59c6c072
Juha Paananen <juha.paananen@gmail.com>
2014-11-01 11:16:14 +0200

    run code on alt-enter

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-Clone. Open index.html. Enjoy.
+Build and run
+
+    npm install
+    node server.js
+    open http://localhost:3000
+
+Key bindings
+
+    Alt-Enter = run code
--- bacon-preso.js
+++ bacon-preso.js
@@ -1,26 +1,32 @@
-function keyUps(keyCode) { 
+function keyUps(keyCode, modifiers) { 
   return $(document).asEventStream("keyup")
-                    .filter(function(e) { 
-                      return e.keyCode == keyCode 
-                    }) 
+                    .filter(filterKeys(keyCode, modifiers)) 
 }
 
-function keyDowns(keyCode) { 
+function keyDowns(keyCode, modifiers) { 
   return $(document).asEventStream("keydown")
-                    .filter(function(e) { 
-                      return e.keyCode == keyCode 
-                    }) 
+                    .filter(filterKeys(keyCode, modifiers)) 
+}
+
+function filterKeys(keyCode, modifiers) {
+  var knownModifiers = ["alt", "shift", "meta"]
+  return function(e) {
+    return e.keyCode = keyCode && (!modifiers || _.all(knownModifiers, function(m) {
+      var modifierActive = e[m + "Key"]
+      var expectedActive = _.contains(modifiers, m)
+      return modifierActive == expectedActive
+    }))
+  }
 }
 
 function keyState(keyCode, value) {
   return keyDowns(keyCode).map([value]).merge(keyUps(keyCode).map([])).toProperty([]).skipDuplicates()
 }
 
-$(function() {
-  // activity: EventStream
-  var activity = $(window).asEventStream('mousemove keydown')
-  // timeout: EventStream
-  var timeout = activity.throttle(3000)
-  // active: Property Bool
-  active = activity.awaiting(timeout)
-})
+function runCode() {
+  var code = $("code:visible")
+  code.blur()
+  eval(code.text())
+}
+
+keyUps(10, ["alt"]).doAction(".preventDefault").onValue(runCode)
